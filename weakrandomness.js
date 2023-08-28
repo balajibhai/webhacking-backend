86058672a22fa2b90ed6c692d252a33d05d7da7f
Balaji Narasimhan <balaji@Balajis-MacBook-Pro.local>
2023-08-28 15:06:20 -0400

    done till dummy reset password

--- weakrandomness.js
+++ weakrandomness.js
@@ -7,8 +7,15 @@ const connection = require("./database");
 app.use(express.json());
 app.use(cors());
 
+const hashPassword = (password) => {
+  const saltRounds =
+    "$2b$04$ui0Zre2YtRmY0Zz1rdIQeutWHXUnLff/ujyZMhc9AW4.3uREH36cW";
+  return bcrypt.hash(password, saltRounds);
+};
+
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
+  const passwdHash = await hashPassword(password);
   const insertQuery = `insert into users (email, password) values (?, ?)`;
 
   if (!email || !password) {
@@ -18,15 +25,22 @@ app.post("/register", async (req, res) => {
   }
 
   try {
-    const saltRounds = 2;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    console.log("hashedPassword: ", hashedPassword);
-    connection.query(insertQuery, [email, password], (insertErr, inserRes) => {
-      connection.end();
-      if (insertErr) {
-        console.log("Error inserting the record: ", insertErr);
+    connection.getConnection((getConnectionError, connectionQuery) => {
+      if (getConnectionError) {
+        console.log("Error", getConnectionError);
       } else {
-        console.log("Record inserted successfully", inserRes);
+        connectionQuery.query(
+          insertQuery,
+          [email, passwdHash],
+          (insertErr, inserRes) => {
+            if (insertErr) {
+              console.log("Error inserting the record: ", insertErr);
+            } else {
+              console.log("Record inserted successfully", inserRes);
+            }
+          }
+        );
+        connectionQuery.release();
       }
     });
 
@@ -37,6 +51,52 @@ app.post("/register", async (req, res) => {
       .status(500)
       .json({ message: "An error occurred while registering." });
   }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const selectPwdQuery = `select password from users where email=?`;
+  const passwdHash = await hashPassword(password);
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Email and password are required." });
+  }
+  try {
+    connection.getConnection((getConnectionError, connectionQuery) => {
+      if (getConnectionError) {
+        console.log("Error", getConnectionError);
+      } else {
+        connectionQuery.query(
+          selectPwdQuery,
+          [email],
+          (selectErr, selectRes) => {
+            if (!selectErr && passwdHash === selectRes[0]?.password) {
+              return res
+                .status(200)
+                .send({ message: "logged in successfully" });
+            } else {
+              return res
+                .status(500)
+                .send({ message: "password or email mismatch" });
+            }
+          }
+        );
+        connectionQuery.release();
+      }
+    });
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while logging in" });
+  }
+});
+app.get("/resetpassword", (req, res) => {
+  res.status(200).send({
+    message: "Reset password link is this fjkanfanllqeqwejlqje",
+  });
 });
 
 app.listen(3001, () => {
